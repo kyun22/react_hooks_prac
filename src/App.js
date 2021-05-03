63882cbfaa417d66216ecae1141186a6c62b71d8
shlee <seokhyun90@gmail.com>
2021-05-03 23:10:02 +0900

    20210503

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useInput} from "./useInput";
 
 function App() {
+    const maxLen = value => value.length <= 10;
+    const name = useInput("Mr. Shlee", maxLen);
+    const email = useInput("@");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
+        <input type="text" {...name}/>
     </div>
   );
 }
